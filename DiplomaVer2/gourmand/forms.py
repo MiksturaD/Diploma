033539b6284df6128f18e27fb2a31623e390c520
MiksturaD <162081822+MiksturaD@users.noreply.github.com>
2025-01-21 16:13:29 +0700

    20.01.2025

---
+++ DiplomaVer2/gourmand/forms.py
@@ -0,0 +1,167 @@
+from django import forms
+from django.contrib.auth.forms import UserChangeForm
+
+from gourmand.models import User, Review, Place, Event
+
+
+class SignupForm(forms.ModelForm):
+  confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+    'class': 'form-control',
+    'id': 'floatingPasswordConfirm',
+    'placeholder': 'Подтверждение пароля',
+    'required': True
+  }))
+
+  class Meta:
+    model = User
+    fields = ['first_name', 'last_name', 'username', 'email', 'password']
+
+    widgets = {
+      'first_name': forms.TextInput(attrs={
+        'class': 'form-control',
+        'id': 'floatingTitle',
+        'placeholder': 'Имя',
+        'required': True
+      }),
+      'last_name': forms.TextInput(attrs={
+        'class': 'form-control',
+        'id': 'floatingLastname',
+        'aria-label': 'Фамилия',
+        'placeholder': 'Фамилия',
+        'required': True
+      }),
+      'username': forms.TextInput(attrs={
+        'class': 'form-control',
+        'id': 'floatingUsername',
+        'placeholder': 'Логин пользователя',
+        'aria-label': 'Логин пользователя',
+        'required': True
+      }),
+      'email': forms.EmailInput(attrs={
+        'class': 'form-control',
+        'id': 'floatingEmail',
+        'placeholder': 'Почта',
+        'required': True
+      }),
+      'password': forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'id': 'floatingPassword',
+        'placeholder': 'Пароль',
+        'required': True
+      }),
+    }
+
+  def clean(self):
+    cleaned_data = super().clean()
+    password = cleaned_data.get('password')
+    confirm_password = cleaned_data.get('confirm_password')
+    if password != confirm_password:
+      raise forms.ValidationError('Пароли не совпадают!')
+    return cleaned_data
+
+class CustomUserChangeForm(UserChangeForm):
+  class Meta:
+      model = User
+      fields = ['first_name', 'last_name', 'username', 'email']
+
+
+class ReviewCreateForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ['name', 'review_date', 'description', 'place']
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'required': True
+            }),
+            'review_date': forms.DateField(attrs={
+                'class': 'form-control',
+                'required': True
+            }),
+            'description': forms.TextInput(attrs={
+                'class': 'form-select',
+                'required': True
+            }),
+            'place': forms.TextInput(attrs={
+                'class': 'form-control',
+                'required': True
+            }),
+        }
+        labels = {
+            'name': 'Название отзыва',
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ReviewCreateForm, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update({'aria-label': 'Название отзыва'})
+        self.fields['review_date'].widget.attrs.update({'aria-label': 'Дата отзыва'})
+        self.fields['description'].widget.attrs.update({'aria-label': 'Детали отзыва'})
+        self.fields['place'].widget.attrs.update({'aria-label': 'Наименование заведения'})
+
+
+class PlaceCreateForm(forms.ModelForm):
+    class Meta:
+        model = Place
+        fields = ['name', 'description', 'place_email', 'location', 'rating']
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'required': True
+            }),
+            'description': forms.TextInput(attrs={
+                'class': 'form-control',
+                'required': True
+            }),
+            'place_email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'required': True
+            }),
+            'location': forms.TextInput(attrs={
+                'class': 'form-control',
+                'required': True
+            }),
+            'rating': forms.TextInput(attrs={
+                'class': 'form-control',
+                'required': True
+            }),
+        }
+        labels = {
+            'name': 'Название отзыва',
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(PlaceCreateForm, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update({'aria-label': 'Название отзыва'})
+        self.fields['description'].widget.attrs.update({'aria-label': 'Описание заведения'})
+        self.fields['place_email'].widget.attrs.update({'aria-label': 'Емайл заведения'})
+        self.fields['location'].widget.attrs.update({'aria-label': 'Адрес заведения'})
+        self.fields['rating'].widget.attrs.update({'aria-label': 'Рейтинг заведения'})
+
+
+class EvenCreateForm(forms.ModelForm):
+    class Meta:
+        model = Event
+        fields = ['name', 'description', 'place']
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'required': True
+            }),
+            'description': forms.TextInput(attrs={
+                'class': 'form-control',
+                'required': True
+            }),
+            'place': forms.TextInput(attrs={
+                'class': 'form-control',
+                'required': True
+            }),
+        }
+        labels = {
+            'name': 'Название ивента',
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(EvenCreateForm, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update({'aria-label': 'Название ивента'})
+        self.fields['description'].widget.attrs.update({'aria-label': 'Описание ивента'})
+        self.fields['place'].widget.attrs.update({'aria-label': 'Наименование заведения в котором будет ивент'})
---
+++ DiplomaVer2/gourmand/views.py
@@ -0,0 +1,76 @@
+from django.shortcuts import render
+
+from gourmand.models import Review
+
+
+def index(request):
+  review_list = Review.objects.all()
+  return render(request, 'gourmand/index.html', context={'reviews': review_list})
+
+
+def main(request):
+  return None
+
+
+def edit_profile(request):
+  return None
+
+
+def signup(request):
+  return None
+
+
+def signin(request):
+  return None
+
+
+def signout(request):
+  return None
+
+
+def profile(request):
+  return None
+
+
+def events(request):
+  return None
+
+
+def event(request):
+  return None
+
+
+def create_event(request):
+  return None
+
+
+def places(request):
+  return None
+
+
+def place(request):
+  return None
+
+
+def create_places(request):
+  return None
+
+
+def reviews(request):
+  return None
+
+
+def review(request):
+  return None
+
+
+def create_review(request):
+  return None
+
+
+def about(request):
+  return None
+
+
+def contacts(request):
+  return None

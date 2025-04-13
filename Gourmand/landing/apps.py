564911151f19400d0728c07e98d7421173c7fa64
MiksturaD <162081822+MiksturaD@users.noreply.github.com>
2025-04-13 12:59:13 +0700

    Update apps.py

--- Gourmand/landing/apps.py
+++ Gourmand/landing/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 class LandingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'landing'
+    name = 'Gourmand.landing'
 
     def ready(self):
         import landing.signals  # Подключаем сигналы

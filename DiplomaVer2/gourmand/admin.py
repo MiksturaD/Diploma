a703dfaa155324229e5cb7f32a8d5250c6263960
MiksturaD <162081822+MiksturaD@users.noreply.github.com>
2025-04-13 12:32:02 +0700

    del old projects

--- DiplomaVer2/gourmand/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from gourmand.models import Review, Place, Gourmand, Event, User
-
-admin.site.register(Place)
-admin.site.register(Review)
-admin.site.register(Gourmand)
-admin.site.register(Event)
-admin.site.register(User)
--- The_Project_Gourmand/gourmand/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GourmandConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'gourmand'

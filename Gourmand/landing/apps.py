a3abdfb7a9ed8e57e75f6996b1dd156824f79fdb
MiksturaD <162081822+MiksturaD@users.noreply.github.com>
2025-04-13 12:51:20 +0700

    Update apps.py

--- Gourmand/landing/apps.py
+++ Gourmand/landing/apps.py
@@ -1,10 +1,5 @@
 from django.apps import AppConfig
 
-
-class GourmandConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'landing'
-
 class LandingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'landing'

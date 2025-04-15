e6e44c542f1114d90287c1da147e4c1c09b18ee2
MiksturaD <162081822+MiksturaD@users.noreply.github.com>
2025-04-15 21:04:19 +0700

    Update settings.py

--- Gourmand/settings.py
+++ Gourmand/settings.py
@@ -140,10 +140,6 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
-# Добавьте путь к статическим файлам вашего приложения
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'landing/static'),
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

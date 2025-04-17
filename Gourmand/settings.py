3dfede80ad9f4030a78b70d6db7685ccd6f2eec1
MiksturaD <162081822+MiksturaD@users.noreply.github.com>
2025-04-17 20:17:27 +0700

    Update settings.py

--- Gourmand/settings.py
+++ Gourmand/settings.py
@@ -25,7 +25,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = os.getenv('DEBUG') == 'False'
+DEBUG = os.getenv('DEBUG') == 'True'
 # SECURITY WARNING: don't run with debug turned on in production!
 
 

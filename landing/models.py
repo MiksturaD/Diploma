ce462ba5ddb19a23431fbd25052806ac30ca54c5
MiksturaD <162081822+MiksturaD@users.noreply.github.com>
2025-04-30 13:09:18 +0700

    test

--- landing/models.py
+++ landing/models.py
@@ -33,7 +33,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=30, blank=True)
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default="gourmand", db_index=True)  
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default="gourmand", db_index=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     slug = models.SlugField(max_length=100, blank=True, unique=True, verbose_name="Слаг")
